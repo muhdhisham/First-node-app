75eafdec213d81ff576d9f2f1af13d376d856930
muhdhisham <muhdhisham034@gmail.com>
2022-06-25 18:53:22 +0530

    made using hbs

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
+const expressHbs = require('express-handlebars')
 const users = [];
 
-app.set('view engine', 'pug');
+
+// app.engine('hbs',expressHbs.engine());
+
+app.engine('hbs',expressHbs.engine({ layoutsDir:'views/layouts/', defaultLayout: 'main-layout', extname:'hbs' }));
+app.engine('hbs', expressHbs.engine({ defaultLayout: 'main-layout', extname: 'hbs' }));
+
+app.set('view engine', 'hbs');
 app.set('views','views');
 
 
@@ -15,22 +21,19 @@ app.get('/', (req,res,next) => {
     res.render('index', {pageTitle:'Add User'})
 });
 
-app.get('/users', (req,res,next) => {
-    res.render('users', {pageTitle: 'Users', users: users})
-});
+app.get('/users', (req, res, next) => {
+    res.render('users', {
+      pageTitle: 'User',
+      users: users,
+      hasUsers: users.length > 0
+    });
+  }); 
 
 app.post('/add-user', (req, res, next) => {
     users.push({name: req.body.username});
+    console.log(users)
     res.redirect('/users');
 });
-
-
-
-
-
-
-
-
 
 
 
